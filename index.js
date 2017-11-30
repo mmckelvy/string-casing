73b3fa7fa21f991503db64a0f0cae70e25f80607
Mark McKelvy <mmckelvy@gmail.com>
2017-11-30 12:48:18 -0800

    export the module

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
   snakeCase: require('./lib/snake-case'),
   camelCase: require('./lib/camel-case'),
   camelCaseSpace: require('./lib/camel-case-space'),
+  camelCaseDash: require('./lib/camel-case-dash'),
 }
